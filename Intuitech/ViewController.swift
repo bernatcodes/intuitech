1b4c2eb1701c888e41f7901517b34a05d98193e4
Bernat Szabo <szabobernat@icloud.com>
2023-09-06 20:26:22 +0200

    Initial Commit

---
+++ Intuitech/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Intuitech
+//
+//  Created by Bernát Szabó on 2023. 09. 06..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ IntuitechUITests/IntuitechUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  IntuitechUITestsLaunchTests.swift
+//  IntuitechUITests
+//
+//  Created by Bernát Szabó on 2023. 09. 06..
+//
+
+import XCTest
+
+final class IntuitechUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
